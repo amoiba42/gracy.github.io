162bbcbc4be45d3cd9e39a407797afb6335c0d26
gracy_garg6516 <143736895+amoiba42@users.noreply.github.com>
2024-02-06 23:43:28 +0530

    Add files via upload

---
+++ bio.js
@@ -0,0 +1,6 @@
+function toggleMode() 
+{
+    var body = document.body;
+    body.classList.toggle("dark-mode");
+   
+}
---
+++ edu.js
@@ -0,0 +1,8 @@
+function toggleMode() 
+{
+    var flag=0;
+
+    var body = document.body;
+    body.classList.toggle("dark-mode");
+
+}
